929cbcd7c79e49deb6f4a91f3149f25ea72cb591
Brandon <Brandon@NubCenter.attlocal.net>
2017-09-30 21:47:53 -0500

    Lab 2: Cleaned Up FoodServiceTipCalculator

--- src/dip/lab2/FoodServiceTipCalculator.java
+++ src/dip/lab2/FoodServiceTipCalculator.java
@@ -8,57 +8,37 @@ package dip.lab2;
  *
  * @author your name goes here
  */
-public class FoodServiceTipCalculator {
-    private static final double MIN_BILL = 0.00;
-    private static final String BILL_ENTRY_ERR =
-            "Error: bill must be greater than or equal to " + MIN_BILL;
-    private static final double GOOD_RATE = 0.20;
-    private static final double FAIR_RATE = 0.15;
-    private static final double POOR_RATE = 0.10;
-
+public class FoodServiceTipCalculator implements TipCalculator{
     private double bill;
-    public enum ServiceQuality {
-        GOOD, FAIR, POOR
-    }
-    private ServiceQuality serviceQuality;
-
-    public FoodServiceTipCalculator(ServiceQuality q, double billAmt) {
-        this.setServiceRating(q);
-        this.setBill(billAmt);
+    private double tipPercent; 
+    
+    public FoodServiceTipCalculator(double tipPercent, double billAmt) {
+        setTipPercent(tipPercent);
+        setBill(billAmt);
     }
 
-    public double getTip() {
-        double tip = 0.00; // always initialize local variables
-
-        switch(serviceQuality) {
-            case GOOD:
-                tip = bill * GOOD_RATE;
-                break;
-            case FAIR:
-                tip = bill * FAIR_RATE;
-                break;
-            case POOR:
-                tip = bill * POOR_RATE;
-                break;
-        }
-
-        return tip;
+    public final double getTip() {
+        return bill * tipPercent;
     }
 
     public final void setBill(double billAmt) {
-        if(billAmt < MIN_BILL) {
-            throw new IllegalArgumentException(BILL_ENTRY_ERR);
+        if(billAmt < 0) {
+            throw new IllegalArgumentException("Bill Amount May Not Be Negitive");
         }
         bill = billAmt;
     }
 
-    public final void setServiceRating(ServiceQuality q) {
-        // No need to validate because enums provide type safety!
-        serviceQuality = q;
+    public final double getBill(){
+        return bill;
+    }
+    
+    public final void setTipPercent(double tipPercent) {
+        if(tipPercent < 0) throw new IllegalArgumentException("Tip Percent May Not Be Negitive");
+        this.tipPercent = tipPercent;
     }
 
-    public ServiceQuality getServiceQuality() {
-        return serviceQuality;
+    public final double getTipPercent() {
+        return tipPercent;
     }
 
 }
